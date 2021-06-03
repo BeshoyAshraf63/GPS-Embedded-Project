49db8ae1611ce7b976fa7559fb6fe15cacdb7ff1
BeshoyAshraf63 <85255186+BeshoyAshraf63@users.noreply.github.com>
2021-06-03 16:15:27 +0200

    added rest of getDistance() function

--- main.c
+++ main.c
@@ -18,7 +18,17 @@ double getDistance(double p1[],double p2[]){
    double lon1=p1[1];
    double lat2=p2[0];
    double lon2=p2[1];
+	 double x1 = lat1*(PI/180);
+   double x2 = lat2*(PI/180);
+   double dx = (lat2-lat1)*(PI/180);
+   double dy = (lon2-lon1)*(PI/180);
 
+   double a=sin((dx/2))*sin((dx/2))+cos((x1))*cos((x2))
+         *sin((dy/2))*sin((dy/2));
+   double c=2*atan2(sqrt(a),sqrt((1-a)));
+   double d=(R*c);
+
+     return d;
 }
 
 
