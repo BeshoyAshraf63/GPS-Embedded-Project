68af7cd6ba03419a446d2be5b53d6310fcff7341
Mark Amgad <64510804+Mark-Amgad@users.noreply.github.com>
2021-06-04 21:24:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -31,15 +31,18 @@ void systick_init(){
 }
 void portF_led_init()
 {
-		SYSCTL_RCGC2_R |= 0x00000020;;   /* enable clock to GPIOF */
+		SYSCTL_RCGCGPIO_R |= 0x20;   /* enable clock to GPIOF */
+    while((SYSCTL_RCGC2_R & 0x20)==0);
+
+    GPIO_PORTF_LOCK_R = 0x4C4F434B; // unlock
+		GPIO_PORTF_CR_R = 0x0F;
 		GPIO_PORTF_AFSEL_R =0x00; // disable alternative functions
 		GPIO_PORTF_PCTL_R = 0;
 		GPIO_PORTF_DIR_R = 0x0E;        // set pins 1,2,3 as output
 
 		GPIO_PORTF_AMSEL_R = 0;
 		GPIO_PORTF_DEN_R = 0x0F;         /* set PORTF pins 1 to 4 as digital pins */
-		GPIO_PORTF_LOCK_R = 0x4C4F434B; // unlock
-		GPIO_PORTF_CR_R = 0x0F;
+
 		GPIO_PORTF_PUR_R = 0x01;         /* enable pull up for pin 0 */
 
 }
