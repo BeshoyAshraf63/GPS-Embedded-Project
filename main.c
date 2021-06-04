15a1e64164e620a4fe2a2f24606aa47471e8fd54
susanna-michael-faheem <85255041+susanna-michael-faheem@users.noreply.github.com>
2021-06-04 19:28:55 +0200

    the rest of systick handler function
    it will run every 5 msec

--- main.c
+++ main.c
@@ -12,7 +12,11 @@
 uint8_t  numbersArr[10] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f};
 uint32_t distance = 0;
 uint8_t	currentnumber=0;
-
+ uint8_t segment=0;
+ uint8_t Arr2[10]={0,1,2,3,4,5,6,7,8,9};     //an array for knowing what is the current number 
+ uint32_t Arr3[3]={1000,100,10};       //to split the distance into digits  
+ 
+ 
 
 
 void SystemInit(void){
@@ -100,6 +104,16 @@ void SysTick_Handler (void)
 		 GPIO_PORTB_DATA_R= numbersArr[1];
 	 }
 	else{
-
+	uint8_t n=0;
+	currentnumber=distance/Arr3[n];
+    for(uint8_t i=0; i<10;i++){
+			if(currentnumber==Arr2[i]){
+			GPIO_PORTE_DATA_R=segment+1;
+			GPIO_PORTB_DATA_R=numbersArr[Arr2[i]];
+		} 
+		segment++;
+		n++;
+	                
+	
 	}
-}
+}}
