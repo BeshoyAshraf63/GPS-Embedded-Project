67aede6aab4a87f925081d04b418b7765ac93489
Mark Amgad <64510804+Mark-Amgad@users.noreply.github.com>
2021-06-04 19:16:39 +0200

    INIT_LEDS
    port F initialization.

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "stdint.h"
-#include <string.h>	
+#include <string.h>
 #include <stdlib.h>
 #include <math.h>
 #include "tm4c123gh6pm.h"
 
 #define PI 3.14159265359
 #define R 6372795
-#define DISTINATION_OFFSET 10		
+#define DISTINATION_OFFSET 10
 
 
 uint8_t  numbersArr[10] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f};
@@ -19,18 +19,34 @@ void SystemInit(void){
 	NVIC_CPAC_R |= 0x00F00000;		//enable FPU - Floating-point numbers
 }
 
+void portF_led_init()
+{
+		SYSCTL_RCGC2_R |= 0x00000020;;
+		GPIO_PORTF_AFSEL_R =0x00;
+		GPIO_PORTF_PCTL_R = 0;
+    GPIO_PORTF_DIR_R = 0x0E;
+
+		GPIO_PORTF_AMSEL_R = 0;
+    GPIO_PORTF_DEN_R = 0x0F;         
+		GPIO_PORTF_LOCK_R = 0x4C4F434B;
+		GPIO_PORTF_CR_R = 0x0F;
+    GPIO_PORTF_PUR_R = 0x01;
+
+}
+
+
 void portE_enables_init (void){
 	SYSCTL_RCGCGPIO_R |=0x10;
 		while (( SYSCTL_PRGPIO_R &0x10) == 0);
 		GPIO_PORTE_LOCK_R=0x4c4f434b ;
 		GPIO_PORTE_CR_R |=0X1E ;
-		GPIO_PORTE_AFSEL_R &=~ 0X1E;		
+		GPIO_PORTE_AFSEL_R &=~ 0X1E;
 		GPIO_PORTE_PCTL_R = 0;
 		GPIO_PORTE_AMSEL_R &=~0X1E;
 		GPIO_PORTE_DEN_R |=0X1E;
 		GPIO_PORTE_DIR_R |=0X1E;
 		GPIO_PORTE_PUR_R |=0X00;
-		
+
 	}
 void portB_segments_init(void){
 	if(SYSCTL_PRGPIO_R & 0xFD)!= SYSCTL_PRGPIO_R){} // checks if already the port is enabled and does nothing
@@ -45,12 +61,12 @@ void portB_segments_init(void){
 			GPIO_PORTB_PCTL_R = 0x00000000; // used as GPIO so PCTL and AMSEL are reset
 			GPIO_PORTB_DIR_R|=0x7F;
 			GPIO_PORTB_PUR_R &=~0x7F;
-			
+
 		}
-	
 
 
-	
+
+
 }
 double getDistance(double p1[],double p2[]){
    double lat1=p1[0];
@@ -77,15 +93,13 @@ int main(void){
 
 void SysTick_Handler (void)
 {
-	  
+
    if (distance>9999){
-		 
+
 		 GPIO_PORTE_DATA_R=0x10;
 		 GPIO_PORTB_DATA_R= numbersArr[1];
 	 }
 	else{
-	
+
 	}
 }
-
-
